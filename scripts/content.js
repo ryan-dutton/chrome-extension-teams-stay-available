27b9837f0adcbc7de54401f352f6d39e34a11235
ryan-dutton <ryan.dutton@gmail.com>
2023-01-13 11:35:24 +0800

    extend timeout to 4 minutes to avoid losing focus whilst typing

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,5 @@
 function start() {
-	var timeout = setTimeout( function() { start(); }, 10000);
+	var timeout = setTimeout( function() { start(); }, 1000*4*60); //every 4 minutes
 	window.focus();
 }
 start();
